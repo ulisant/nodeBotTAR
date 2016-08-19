92c9810ccfe3aafa22e4ae443b38ad183f341c0c
ulisant <Ulises@MacBook-Pro-de-mac.local>
2016-08-19 05:35:02 -0500

    add function of get data complete full3

--- server.js
+++ server.js
@@ -94,8 +94,8 @@ app.post('/tarant/', function (req, res) {
 			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
 		if (event.postback) {
-      sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
 			let text = JSON.stringify(event.postback)
+      sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
       if (text === "Experiencias") {
         data_send.category = 1
         getTitlePost(sender)
