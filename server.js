122eb56fd7c89c450dffdaaf857f01f082a6d663
ulisant <Ulises@MacBook-Pro-de-mac.local>
2016-08-19 02:08:39 -0500

    Add images and card of caution

--- server.js
+++ server.js
@@ -51,6 +51,10 @@ app.post('/tarant/', function (req, res) {
         dataPosts(sender)
         continue
       }
+      if (text === "Prevención") {
+        sendImages(sender)
+        continue
+      }
 			sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
 		}
 		if (event.postback) {
@@ -223,6 +227,133 @@ function dataPosts(sender) {
 			"payload": {
 				"template_type": "generic",
 				"elements": data_post
+			}
+		}
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
+function sendImages(sender) {
+	let messageData = {
+		"attachment": {
+			"type": "template",
+			"payload": {
+				"template_type": "generic",
+				"elements": [{
+					"title": "Marca 01",
+					"subtitle": "ABREN CON CADENA",
+					"image_url": "images/abren_con_cadena.png",
+				}, {
+					"title": "Marca 02",
+					"subtitle": "AQUI NADA",
+					"image_url": "images/aqui_nada.png",
+				}, {
+					"title": "Marca 03",
+					"subtitle": "CASA CARITATIVA",
+					"image_url": "images/casa_caritativa.png",
+				}, {
+					"title": "Marca 04",
+					"subtitle": "CASA DE EMPRESARIO",
+					"image_url": "images/casa_de_empresario.png",
+				}, {
+					"title": "Marca 05",
+					"subtitle": "CASA DESHABITADA",
+					"image_url": "images/casa_deshabitada.png",
+				}, {
+					"title": "Marca 06",
+					"subtitle": "CASA YA ROBADA",
+					"image_url": "images/casa_ya_robada.png",
+				}, {
+					"title": "Marca 07",
+					"subtitle": "CUIDADO POLICIA",
+					"image_url": "images/cuidado_policia.png",
+				}, {
+					"title": "Marca 08",
+					"subtitle": "DE VACACIONES",
+					"image_url": "images/de_vacaciones.png",
+				}, {
+					"title": "Marca 09",
+					"subtitle": "DISPUESTA PARA ROBAR",
+					"image_url": "images/dispuesta_para_robar.png",
+				}, {
+					"title": "Marca 10",
+					"subtitle": "ESTAN FUERA PERO HAY DIFICULTADES",
+					"image_url": "images/_estan_fuera_pero_hay_dificultades.png",
+				}, {
+					"title": "Marca 11",
+					"subtitle": "FACIL POR LA MAÑANA",
+					"image_url": "images/facil_por_la_mañana.png",
+				}, {
+					"title": "Marca 12",
+					"subtitle": "FACIL POR LA NOCHE",
+					"image_url": "images/facil_por_la_noche.png",
+				}, {
+					"title": "Marca 13",
+					"subtitle": "FACIL POR LA TARDE",
+					"image_url": "images/facil_por_la_tarde.png",
+				}, {
+					"title": "Marca 14",
+					"subtitle": "INUTIL INSISTIR",
+					"image_url": "images/inutil_insistir.png",
+				}, {
+					"title": "Marca 15",
+					"subtitle": "MUJER SOLA",
+					"image_url": "images/mumjer_sola.png",
+				}, {
+					"title": "Marca 16",
+					"subtitle": "MUY BUENA",
+					"image_url": "images/Muy_buena.png",
+				}, {
+					"title": "Marca 17",
+					"subtitle": "NADA DE INTERES",
+					"image_url": "images/nada_de_interes.png",
+				}, {
+					"title": "Marca 18",
+					"subtitle": "NIÑOS SOLOS POR LA MAÑANA",
+					"image_url": "images/niños_solos_por_la_mañana.png",
+				}, {
+					"title": "Marca 19",
+					"subtitle": "NIÑOS SOLOS POR LA TARDE",
+					"image_url": "images/niños_solos_por_la_tarde.png",
+				}, {
+					"title": "Marca 20",
+					"subtitle": "OJO HAY PERRO",
+					"image_url": "images/ojo_hay_perro.png",
+				}, {
+					"title": "Marca 21",
+					"subtitle": "ROBAR INVALIDO",
+					"image_url": "images/robar_invalido.png",
+				}, {
+					"title": "Marca 22",
+					"subtitle": "SE PUEDE ROBAR",
+					"image_url": "images/se_puede_robar.png",
+				}, {
+					"title": "Marca 23",
+					"subtitle": "SOLO VIVEN MUJERES",
+					"image_url": "images/solo_viven_mujeres.png",
+				}, {
+					"title": "Marca 24",
+					"subtitle": "USAR PALANCA",
+					"image_url": "images/usar_palanca.png",
+				}, {
+					"title": "Marca 25",
+					"subtitle": "VUELVE PRONTO",
+					"image_url": "images/vuelve_pronto.png",
+				}]
 			}
 		}
 	}
