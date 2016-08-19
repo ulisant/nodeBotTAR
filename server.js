235183020d2127852d9289a8e2652966b75ead56
ulisant <Ulises@MacBook-Pro-de-mac.local>
2016-08-19 00:50:18 -0500

    add breaklines in cards contact3

--- server.js
+++ server.js
@@ -171,7 +171,9 @@ function dataMessage(sender) {
 				"template_type": "generic",
 				"elements": [{
 					"title": "Teléfonos de Contacto",
-					"subtitle": "(775) 75 3 01 57 EXT. 4133 y 4633  \ (775) 75 5 26 86  \ 01 (800) 69 0 74 48",
+					"subtitle": "(775) 75 3 01 57 EXT. 4133 y 4633" +
+          "(775) 75 5 26 86 " +  
+          "01 (800) 69 0 74 48",
 					"image_url": "http://premiumwebfreebies.com/wp-content/uploads/2013/11/flat-email-icon-display.jpg",
 
 				}]
